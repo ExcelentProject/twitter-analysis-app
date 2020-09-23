d3e897742960f84b53bfcbcb17da019fe16cd65c
Jakub Stejskal <xstejs24@gmail.com>
2020-09-23 16:49:52 +0200

    Final changes
    Signed-off-by: Jakub Stejskal <xstejs24@gmail.com>

--- consume-telegram-messages.sh
+++ consume-telegram-messages.sh
@@ -3,4 +3,4 @@ set -v
 
 TOPIC=${1}
 
-kubectl run kafka-consumer -ti --image=strimzi/kafka:latest-kafka-2.6.0 --rm=true --restart=Never -- bin/kafka-console-consumer.sh --bootstrap-server my-cluster-kafka-bootstrap:9092 --topic ${TOPIC} --from-beginning
+kubectl run kafka-consumer -ti --image=strimzi/kafka:0.19.0-kafka-2.5.0 --rm=true --restart=Never -- bin/kafka-console-consumer.sh --bootstrap-server my-cluster-kafka-bootstrap:9092 --topic ${TOPIC} --from-beginning
--- produce-telegram-messages.sh
+++ produce-telegram-messages.sh
@@ -3,4 +3,4 @@ set -v
 
 TOPIC=${1}
 
-kubectl run kafka-producer -ti --image=strimzi/kafka:latest-kafka-2.6.0 --rm=true --restart=Never -- bin/kafka-console-producer.sh --bootstrap-server my-cluster-kafka-bootstrap:9092 --topic ${TOPIC}
+kubectl run kafka-producer -ti --image=strimzi/kafka:0.19.0-kafka-2.5.0 --rm=true --restart=Never -- bin/kafka-console-producer.sh --bootstrap-server my-cluster-kafka-bootstrap:9092 --topic ${TOPIC}
