b7f1f5ef4404e2a7ea79cb54cad0bc0c108a13d4
Jakub Stejskal <xstejs24@gmail.com>
2021-12-09 11:10:38 +0100

    Change formating for telegram message
    Signed-off-by: Jakub Stejskal <xstejs24@gmail.com>

--- twitter-message-parser/src/main/java/io/tealc/streams/TopologyProducer.java
+++ twitter-message-parser/src/main/java/io/tealc/streams/TopologyProducer.java
@@ -38,9 +38,9 @@ public class TopologyProducer {
                         String statusUrl = "https://twitter.com/" + value.getUser().getScreenName() + "/status/" + value.getId();
 
                         String telegramTweet = String.format("Hey! There is a new tweet which might interest you!\n" +
-                                        "<strong>Hashtags</strong>: '%s'\n" +
-                                        "<strong>Author</strong>: %s\n" +
-                                        "<strong>URL</strong>: %s\n\n" +
+                                        "*Hashtags*: '%s'\n" +
+                                        "*Author*: %s\n" +
+                                        "*URL*: %s\n\n" +
                                         "%s\n",
                                 tweetHashtags, tweetAuthor, statusUrl, tweetText);
 
